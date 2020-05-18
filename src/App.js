07c99f098852d8e8379d895b230b69996a34cb36
ulfgar94 <62265195+ulfgar94@users.noreply.github.com>
2020-05-18 18:32:00 +0200

    Initialize project using Create React App
    	deleted:    package-lock.json
    	deleted:    package.json
    	modified:   src/App.js
    	modified:   src/index.css
    	modified:   src/index.js
    	new file:   src/logo.svg
    	new file:   src/serviceWorker.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+<<<<<<< HEAD
 import './App.css';
 import Application from './components/Application';
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> 894a745... Initialize project using Create React App
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Application />
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> 894a745... Initialize project using Create React App
     </div>
   );
 }
